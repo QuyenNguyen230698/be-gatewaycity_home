6bd961e9f7e6184a228653922a8db5505ab5e9d3
QuyenNguyen230698 <166916954+QuyenNguyen230698@users.noreply.github.com>
2025-12-11 17:10:18 +0700

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,13 @@
 module.exports = {
-    apps: [
-      {
-        name: "email-worker",
-        script: "./worker.js",
-        env: {
-            NODE_ENV: "production",
-            MONGO_URI: "mongodb://gatewaycity_user:gwchwebdev2025@127.0.0.1:27017/gatewaycity_db?authSource=gatewaycity_db",
-            EMAIL_HOST: "smtp.gmail.com",
-            EMAIL_PORT: "587",
-            EMAIL_USER: "gatewaycityvinhlong@gmail.com",
-            EMAIL_PASS: "phxekquafbvdhqwl"
-        }
+  apps: [
+    {
+      name: "api-gateway",
+      script: "server.js",
+      cwd: "/var/www/api-gateway.gatewaycityvinhlong.vn",
+      watch: false,
+      env: {
+        NODE_ENV: "production"
       }
-    ]
-  };
-  
+    }
+  ]
+}
