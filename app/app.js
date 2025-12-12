9d6188b4be9ce5e15e2f8b5e1c6079f68af281f7
QuyenNguyen230698 <166916954+QuyenNguyen230698@users.noreply.github.com>
2025-12-12 13:57:38 +0700

    update

--- app/app.js
+++ app/app.js
@@ -10,7 +10,8 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: "100mb" }))
+app.use(express.urlencoded({ limit: "100mb", extended: true }))
 app.use(cors());
 // app.use(
 //   cors({
